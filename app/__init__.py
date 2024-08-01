d7f0cbb6f635a015d5221cdc79738f7b5493bb5f
John Walsh <20245424@studentmail.ul.ie>
2024-08-01 22:18:56 +0100

    Fixed file structure to allow project to run

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    # Register blueprints
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
