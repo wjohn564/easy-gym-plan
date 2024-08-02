807f6e952ffbe8d777d0c462c469d4eed8feb115
John Walsh <20245424@studentmail.ul.ie>
2024-08-02 18:28:50 +0100

    Added some page navigation

--- app/auth.py
+++ app/auth.py
@@ -1,13 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+
+from auth_forms import AuthenticationForm, LoginForm
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Handle login logic here
+        return redirect(url_for('main.home'))
+    return render_template('login.html', form=form)
 
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    form = AuthenticationForm()
+    if form.validate_on_submit():
+        # Handle signup logic here
+        return redirect(url_for('main.home'))
+    return render_template('signup.html', form=form)
--- app/main.py
+++ app/main.py
@@ -6,3 +6,8 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/home')
+def home():
+    return render_template('home.html')
